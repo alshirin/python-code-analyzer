5f4f5ef1592b71e524241781b77cd020e544953b
alshirin <alexey.shirin.ge@gmail.com>
2024-01-05 12:34:51 +0400

    add Pylint check

--- code_analyzer.py
+++ code_analyzer.py
@@ -13,11 +13,52 @@ def format_with_black(script_name):
     return run_command(f"black {script_name}")
 
 
+def lint_with_pylint(script_name):
+    """Lint the script with Pylint and check if the rating is more than 5.0."""
+    try:
+        # Run Pylint and capture the output
+        result = subprocess.run(
+            f"pylint {script_name}", capture_output=True, text=True, shell=True
+        )
+
+        # Check for a successful run
+        if result.returncode == 1:
+            print("Error occurred while running Pylint.")
+            return False
+
+        # Parse the output for the rating
+        output = result.stdout
+        warnings = []
+        for line in output.split("\n"):
+            if line.startswith(f"{script_name}"):
+                warnings.append(line)
+            elif line.startswith("Your code has been rated at"):
+                # Extract the rating value
+                rating_str = line.split()[6]
+                rating = float(rating_str.split("/")[0])
+                if rating >= 8.0:
+                    return True
+                else:
+                    print("rating_str", rating_str)
+                    print("output", output)
+                    print("warnings:\n")
+                    for warn in warnings:
+                        print(warn)
+                    print("\n")
+
+        # If the rating is not found, return False
+        return False
+
+    except subprocess.CalledProcessError:
+        # Handle any error during the Pylint run
+        print("Error occurred while running Pylint.")
+        return False
+
+
 def execute_script(script_name):
     """Execute the given Python script."""
     print("\n\033[1;43mScript Execution ...\033[0m\n")
     run_command(f"python {script_name}")
-    print("\n\033[1;43mScript Execution End\033[0m\n")
 
 
 def main(script_name):
@@ -27,6 +68,11 @@ def main(script_name):
             format_with_black,
             "Black formatting \033[31mFAILED\033[0m",
             "Black formatting \033[32mPASSED\033[0m",
+        ),
+        (
+            lint_with_pylint,
+            "Pylint check \033[31mFAILED\033[0m",
+            "PyLint check \033[32mPASSED\033[0m",
         ),
     ]
 
@@ -43,7 +89,6 @@ def main(script_name):
         for error_message in failed_checks:
             print(error_message)
     else:
-        print("\n\033[1;43mScript Execution ...\033[0m\n")
         execute_script(script_name)
 
 
